6e905852d5fa41740da9a3c73aa5b5d1bc759e4f
ko-he- <koheiyatsushiro@yahoo.co.jp>
2021-01-02 02:43:18 +0900

    fix

--- src/burndownchart.js
+++ src/burndownchart.js
@@ -27,46 +27,50 @@ function csv2array(csvStr) {
         actualLineArray[i] = rowArray[4];
     }
     console.log(sprintArray);
-    console.log(sprintArray[1]);
     console.log(closedLineArray);
     console.log(idealLineArray);
     console.log(estimatedLineArray);
     console.log(actualLineArray);
+
+    genChart(sprintArray, closedLineArray, idealLineArray, estimatedLineArray, actualLineArray);
+}
+
+function genChart(sprintArray, closedLineArray, idealLineArray, estimatedLineArray, actualLineArray){
+    var ctx = document.getElementById("burnDownChart");
+    var burnDownChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: sprintArray.slice(1),
+            datasets: [{
+                    label: '理想線',
+                    data: idealLineArray.slice(1),
+                    borderColor: "rgba(255,0,0,1)",
+                    backgroundColor: "rgba(0,0,0,0)",
+                    spanGaps: true
+                },
+                {
+                    label: '計画線',
+                    data: estimatedLineArray.slice(1),
+                    borderColor: "rgba(0,0,255,1)",
+                    backgroundColor: "rgba(0,0,0,0)",
+                    spanGaps: true
+                },
+                {
+                    label: '実績線',
+                    data: actualLineArray.slice(1),
+                    borderColor: "rgba(0,255,0,1)",
+                    backgroundColor: "rgba(0,0,0,0)",
+                    spanGaps: false
+                }
+            ],
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'バーンダウンチャート'
+            }
+        }
+    });
 }
 
 loadcsv("/GithubBurnDownChart/burndownchart.csv")
-var ctx = document.getElementById("burnDownChart");
-var burnDownChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['8月1日', '8月2日', '8月3日', '8月4日', '8月5日', '8月6日', '8月7日'],
-        datasets: [{
-                label: '理想線',
-                data: [200, null, null, null, null, null, null, 0],
-                borderColor: "rgba(255,0,0,1)",
-                backgroundColor: "rgba(0,0,0,0)",
-                spanGaps: true
-            },
-            {
-                label: '計画線',
-                data: [25, 27, 27, 25, 26, 27, 25, 21],
-                borderColor: "rgba(0,0,255,1)",
-                backgroundColor: "rgba(0,0,0,0)",
-                spanGaps: true
-            },
-            {
-                label: '実績線',
-                data: [20, 10, 10, 10, 10, 10, 10, 10],
-                borderColor: "rgba(0,255,0,1)",
-                backgroundColor: "rgba(0,0,0,0)",
-                spanGaps: true
-            }
-        ],
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'バーンダウンチャート'
-        }
-    }
-});
